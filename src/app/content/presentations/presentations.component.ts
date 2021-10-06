c6e09c06231270d458d7a0cc32c4e92d15525ae5
Mohit Kanwar <m.m.kanwar@gmail.com>
2021-10-06 11:27:22 +0530

    adding loader

--- src/app/content/presentations/presentations.component.ts
+++ src/app/content/presentations/presentations.component.ts
@@ -9,13 +9,16 @@ import config from '../../../assets/data/presentations.json';
 export class PresentationsComponent implements OnInit {
 
   presentationsList: Presentation[];
+  showDeck = false;
   selectedIndex;
   previousIndex;
   nextIndex;
+  screenSize: number;
   constructor(private sanitizer: DomSanitizer) {
     this.presentationsList = new Array();
   }
   ngOnInit(): void {
+    this.screenSize = window.innerWidth;
     this.selectedIndex = 0;
     config.presentationList.forEach(element => {
       this.presentationsList.push(
@@ -27,30 +30,47 @@ export class PresentationsComponent implements OnInit {
     });
     this.previousIndex = this.presentationsList.length - 1;
     this.nextIndex = this.selectedIndex + 1;
+    this.showDeck = true;
   }
   selectNext(): void {
-    if (this.selectedIndex === this.presentationsList.length - 1) {
-      this.selectedIndex = 0;
+    this.showDeck = false;
+    this.selectedIndex = this.selectedIndex === this.presentationsList.length - 1 ?
+       0 : this.selectedIndex + 1;
+    this.setupPrevAndNext();
+  }
+  private setupPrevAndNext() {
+    if (this.selectedIndex === 0) {
       this.previousIndex = this.presentationsList.length - 1;
       this.nextIndex = this.selectedIndex + 1;
-    } else {
-      this.selectedIndex++;
-      this.previousIndex = this.selectedIndex - 1;
-      this.nextIndex = this.selectedIndex + 1;
-    }
-  }
-  selectPrevious(): void {
-    if (this.selectedIndex === 0) {
-      this.selectedIndex = this.presentationsList.length - 1;
+    } else if (this.selectedIndex === this.presentationsList.length - 1) {
       this.previousIndex = this.selectedIndex - 1;
       this.nextIndex = 0;
     } else {
-      this.selectedIndex--;
       this.previousIndex = this.selectedIndex - 1;
       this.nextIndex = this.selectedIndex + 1;
     }
   }
 
+  selectPrevious(): void {
+    this.showDeck = false;
+    this.selectedIndex = this.selectedIndex === 0 ?
+      this.presentationsList.length - 1 : this.selectedIndex - 1;
+    this.setupPrevAndNext();
+  }
+  onResize(event) {
+    this.screenSize = event.target.innerWidth;
+    console.log(this.screenSize);
+  }
+
+  onMyFrameLoad(event) {
+    console.log('Frame loaded');
+    console.log(event);
+    this.showDeck = true;
+  }
+  onMyFrameChange(event) {
+    console.log('Frame Changed');
+    console.log(event);
+  }
 }
 
 class Presentation {
