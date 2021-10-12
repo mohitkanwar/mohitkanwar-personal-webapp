b509281e562fdbc9829500cb63dda17ffea55ae1
Mohit Kanwar <m.m.kanwar@gmail.com>
2021-10-12 10:54:57 +0530

    added version management and display code

--- src/app/shared/layout/footer.component.ts
+++ src/app/shared/layout/footer.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { version } from '../../../../package.json';
 @Component({
   selector: 'app-layout-footer',
   templateUrl: './footer.component.html',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
   today: number = Date.now();
+  public version: string = version;
 }
