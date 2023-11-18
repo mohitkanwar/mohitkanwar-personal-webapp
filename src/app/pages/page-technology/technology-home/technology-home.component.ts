e6e2e3f994ad5ff645b45005d9d24865c48f5bc9
Mohit Kanwar <Mohit.Kanwar@Xebia.com>
2023-11-18 18:44:49 +0530

    home page pagintion

--- src/app/pages/page-technology/technology-home/technology-home.component.ts
+++ src/app/pages/page-technology/technology-home/technology-home.component.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 interface BlogData {
-  blogs: [{ fileName: "string", publishDate: Date}];
+  blogs: [{ fileName: "string", publishDate: Date }];
 }
 
 @Component({
@@ -10,35 +10,62 @@ interface BlogData {
   templateUrl: './technology-home.component.html',
   styleUrls: ['./technology-home.component.css']
 })
-export class TechnologyHomeComponent implements OnInit{
-techblogs : string[] = [
-  "technology/working-with-global-teams",
-  "technology/fido",
-  "technology/the-technology-of-modern-banks",
-  "technology/technologies",
-  "technology/large-language-models",
-  "technology/crafting-api-magic"
-]
-constructor(private http: HttpClient) {}
+export class TechnologyHomeComponent implements OnInit {
+  initialDisplayCount = 5;
+  loadCount = 5;
+  techblogs: string[] = [
+    "technology/working-with-global-teams",
+    "technology/fido",
+    "technology/the-technology-of-modern-banks",
+    "technology/technologies",
+    "technology/large-language-models",
+    "technology/crafting-api-magic"
+  ]
+  allBlogs: string[] = [];
+  constructor(private http: HttpClient) { }
 
-ngOnInit(): void {
-  this.http.get<BlogData>('../../../assets/blogs/technology/home.json').subscribe(
-    (data) => {
-      this.techblogs = data.blogs.map(blogItem => {
-        return "technology/"+ blogItem.fileName
-      });
-    },
-    (error) => {
-      console.error('Error fetching JSON file', error);
-      this.techblogs = [
-        "technology/working-with-global-teams",
-        "technology/fido",
-        "technology/the-technology-of-modern-banks",
-        "technology/technologies",
-        "technology/large-language-models",
-        "technology/crafting-api-magic"
-      ]
+  ngOnInit(): void {
+    this.http.get<BlogData>('../../../assets/blogs/technology/home.json').subscribe(
+      (data) => {
+        this.allBlogs = data.blogs.map(blogItem => {
+          return "technology/" + blogItem.fileName
+        });
+        this.techblogs = this.allBlogs.slice(0, this.initialDisplayCount);
+      },
+      (error) => {
+        console.error('Error fetching JSON file', error);
+        this.techblogs = [
+          "technology/working-with-global-teams",
+          "technology/fido",
+          "technology/the-technology-of-modern-banks",
+          "technology/technologies",
+          "technology/large-language-models",
+          "technology/crafting-api-magic"
+        ]
+      }
+    );
+  }
+  loadMore(): void {
+    console.log("load more")
+    if (this.techblogs.length < this.allBlogs.length) {
+      const currentlyDisplayed = this.techblogs.length;
+      const remainingData = this.allBlogs.slice(currentlyDisplayed, currentlyDisplayed + this.loadCount);
+      this.techblogs = this.techblogs.concat(remainingData);
+
     }
-  );
-}
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: any) {
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    const windowHeight = document.documentElement.clientHeight || window.innerHeight;
+    const documentHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
+
+    const currentPosition = scrollPosition + windowHeight;
+    const buffer = 10; // You can adjust the buffer value as needed
+
+    if (currentPosition >= documentHeight - buffer) {
+      this.loadMore();
+    }
+  }
 }
